355cbc45183aba012136d940e0594e7d55729f38
Stephanie-Kibet <54350971+Stephanie-Kibet@users.noreply.github.com>
2020-04-28 17:16:21 +0300

    Add files via upload

---
+++ labs/DBConnector.php
@@ -0,0 +1,26 @@
+<?php 
+	define('DB_SERVER', 'localhost');
+	define('DB_USER', 'root');
+	define('DB_PASS', '');
+	define('DB_NAME', 'btc3205');
+	/**
+	 * 
+	 */
+	class DBConnector
+	{
+
+		public $link;
+		public $conn;
+		function __construct()
+		{
+
+			$this->conn = new mysqli(DB_SERVER, DB_USER, DB_PASS, DB_NAME) or die("Error:" .mysqli_error($this->conn));
+			
+			return $this->conn;
+		}
+		   function closeDatabase(){
+			return mysqli_close($this->conn);
+			}
+		}
+	
+ ?>
